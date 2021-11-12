bb30fe80112609e229fa96306eaad9be2a17be33
Daniella Fernandes <fernandesdaniella@hotmail.com>
2021-11-12 16:21:23 +0000

    Created Lists controller

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,27 @@
+class ListsController < ApplicationController
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render 'new'
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
